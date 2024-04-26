211fb56af10248c3e12a9fa0c614c0087f794822
Priyam <priyamsharma2704@gmail.com>
2024-04-26 17:39:45 -0400

    showing data from local source

--- gridApp/src/Components/Grid.jsx
+++ gridApp/src/Components/Grid.jsx
@@ -1,55 +1,98 @@
 import React, { useState } from "react";
-import JqxGrid from 'jqwidgets-scripts/jqwidgets-react-tsx/jqxgrid';
+import JqxGrid from "jqwidgets-scripts/jqwidgets-react-tsx/jqxgrid";
 import "jqwidgets-scripts/jqwidgets/styles/jqx.base.css";
 import "jqwidgets-scripts/jqwidgets/styles/jqx.material.css";
 
-function Grid()
-{
+function Grid() {
     const [user, setUser] = useState(1);
-    const source = {
-        datafield:[
-            {name:"userId"},
-            {name:"id"},
-            {name:"title"}
-        ],
-        data:{
-            userId:user
+
+    const data = [
+        {
+            Nodes: "3,711",
+            Elements: "720",
+            SolutionTime: "3s ",
+            RAMUsed: "0.53 GB",
+            IORead: "0.00 GB",
+            IOWrite: "0.00 GB",
+            ElementLoadBalanceRatio: "1.241",
+            "SMP/DMP": "DMP",
+            CoresUsed: "4",
+            DateAndTimeOfRun: "03/18/2024 03:10 PM",
         },
-        datatype:"json",
-        id:"id",
-        url:"https://jsonplaceholder.typicode.com/albums"
+        {
+            Nodes: "3,711",
+            Elements: "720",
+            SolutionTime: "9s ",
+            RAMUsed: "0.79 GB",
+            IORead: "0.00 GB",
+            IOWrite: "0.00 GB",
+            ElementLoadBalanceRatio: "1.345",
+            "SMP/DMP": "DMP",
+            CoresUsed: "6",
+            DateAndTimeOfRun: "03/18/2024 03:11 PM",
+        },
+        {
+            Nodes: "3,711",
+            Elements: "720",
+            SolutionTime: "6s ",
+            RAMUsed: "1.30 GB",
+            IORead: "0.00 GB",
+            IOWrite: "0.00 GB",
+            ElementLoadBalanceRatio: "1.391",
+            "SMP/DMP": "DMP",
+            CoresUsed: "10",
+            DateAndTimeOfRun: "03/18/2024 03:57 PM",
+        },
+    ];
+
+    const source = {
+        datafield: [
+            { name: "Nodes" },
+            { name: "Elements" },
+            { name: "SolutionTime" },
+            // {name:"RAMUsed"},
+            // {name:"IORead"},
+            // {name:"IOWrite"},
+            // {name:"ElementLoadBalanceRatio"},
+            // {name:"SMP/DMP"},
+            // {name:"CoresUsed"},
+            // {name:"DateAndTimeOfRun"},
+        ],
+        datatype: "json",
+        localdata: data,
     };
     console.log("setting dataAdapter");
     const adapter = new jqx.dataAdapter(source);
 
-    return(
+    return (
         <>
             <div>this is grid component</div>
             <JqxGrid
-            width="700px"
-            source={adapter}
-            columns={[
-              {
-                text: "userId",
-                datafield: "userId",
-                width: "100"
-              },
-              {
-                text: "id",
-                datafield: "id",
-                width: "100"
-              },
-              {
-                text: "title",
-                datafield: "title"
-              }
-            ]}
-            pageable={true}
-            autoheight={true}
-            sortable={true}
-            theme="material"/>
+                width="700px"
+                source={adapter}
+                columns={[
+                    {
+                        text: "Nodes",
+                        datafield: "Nodes",
+                        width: "100",
+                    },
+                    {
+                        text: "Elements",
+                        datafield: "Elements",
+                        width: "100",
+                    },
+                    {
+                        text: "SolutionTime",
+                        datafield: "SolutionTime",
+                    },
+                ]}
+                pageable={true}
+                autoheight={true}
+                sortable={true}
+                theme="material"
+            />
         </>
-    )
+    );
 }
 
 export default Grid;
