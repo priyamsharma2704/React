a300f7d2518c145e9aec0fff3aaab459dbc24b45
Priyam <priyamsharma2704@gmail.com>
2024-04-29 13:23:32 -0400

    Chart code added

---
+++ gridApp/src/Components/Tab.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Grid from './Grid.jsx';
+import Chart from './Chart.jsx';
+import JqxTabs from 'jqwidgets-scripts/jqwidgets-react-tsx/jqxtabs';
+function Tab()
+{
+    return(
+        <>
+            <div id="container">
+                <JqxTabs
+                    width={'1000px'}
+                    height={'700px'}
+                    position={"top"}
+                    animationType={"none"}
+                    selectionTracker={false}
+                >
+                    <ul>
+                        <li>Tab1</li>
+                        <li>Tab2</li>
+                    </ul>
+                    <div>
+                        <Grid></Grid>
+                        {/* <div className='test1'>
+                            <Chart></Chart>
+                        </div> */}
+                    </div>
+                    
+                    <div>qwqwewq</div>
+                </JqxTabs>
+            </div>
+        </>
+    );
+}
+
+export default Tab;
