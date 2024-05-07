409bd01d4f81655b1e4b820af4c563e3b16a68ef
priyamsharma2704 <priyamsharma2704@gmail.com>
2024-05-07 07:48:53 -0400

    added checkboxes list

--- EasyInstall/src/App.jsx
+++ EasyInstall/src/App.jsx
@@ -1,22 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 //import './App.css'
-import './style.css';
+import "./style.css";
+import AppsLists from "./Components/AppsLists.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div className='font-thin'>hello</div>
-      <div className='font-thin'>hello</div>
-      <div className='font-thin text-red-500'>hello</div>
-      <div className='container mx-auto columns-2'>
-      <input type='text'placeholder='Search...' className='rounded-md pl-3'></input>
-      <img src="./assets/react.svg"></img>
-      </div>
-      
-    </>
-  )
+    return (
+        <>
+        <div className="font-thin">hello</div>
+        <div className="font-thin">hello</div>
+        <div className="font-thin text-red-500">hello</div>
+        <div className="container columns-2 mx-40">
+            <input
+            type="text"
+            placeholder="Search..."
+            className="rounded-md pl-3"
+            ></input>
+            <img className="mx-10" src="./assets/react.svg"></img>
+        </div>
+        <div className=" flex justify-center">
+            <AppsLists></AppsLists>
+        </div>
+        </>
+    );
 }
 
-export default App
+export default App;
