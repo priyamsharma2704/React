1bf1e43ece634eda49a6383cf4ea1cbe678dcf51
priyamsharma2704 <priyamsharma2704@gmail.com>
2024-11-13 11:14:31 -0600

    started project

---
+++ GithubProfileMakeover/client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="container">
+                <div className="form">Form</div>
+                <div className="preview">Preview</div>
+            </div>
+        </>
+    );
+}
+
+export default App;
