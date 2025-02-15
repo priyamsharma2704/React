d80320e201828541277ff7f2c32d4fa33aabb862
priyam sharma <priyamsharma2704@gmail.com>
2025-02-15 11:27:14 -0600

    invt mngmt code changes

--- inventory_management/src/pages/Inward.jsx
+++ inventory_management/src/pages/Inward.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import { useState } from "react";
 
 const Inward = () => {
+    const rowData = {
+        itemName: "",
+        plot: "",
+        qty: "",
+        unit: "",
+        rate: "",
+        weight: "",
+        otherExpense: "",
+        sellerName: "",
+    };
+
+    const [data, setData] = useState([rowData]);
+    const [row, setRow] = useState({});
+
+    function handleInputChange(val, inputField, idx) {
+        console.log(val, inputField, idx);
+    }
+    function handleAddRow() {
+        setData((...prev) => [...data, rowData]);
+    }
+
     return (
         <>
             <div className="inward-page">
@@ -8,16 +29,32 @@ const Inward = () => {
                 <input type="text" placeholder="Receipt Num" />
                 <input type="text" placeholder="Party" />
             </div>
+
             <div className="grid">
-                <input type="text" placeholder="Item Name" />
-                <input type="text" placeholder="Plot #" />
-                <input type="text" placeholder="QTY" />
-                <input type="text" placeholder="Unit" />
-                <input type="text" placeholder="Rate" />
-                <input type="text" placeholder="Weight" />
-                <input type="text" placeholder="Other Expenses" />
-                <input type="text" placeholder="Seller Name" />
+                {data.map((rData, idx) => (
+                    <div key={idx}>
+                        <input
+                            type="text"
+                            onChange={(e) =>
+                                handleInputChange(
+                                    e.target.value,
+                                    "itemName",
+                                    idx
+                                )
+                            }
+                            placeholder="Item Name"
+                        />
+                        <input type="text" placeholder={rData.plot} />
+                        <input type="text" placeholder={rData.qty} />
+                        <input type="text" placeholder={rData.unit} />
+                        <input type="text" placeholder={rData.rate} />
+                        <input type="text" placeholder={rData.weight} />
+                        <input type="text" placeholder={rData.otherExpense} />
+                        <input type="text" placeholder={rData.sellerName} />
+                    </div>
+                ))}
             </div>
+            <button onClick={handleAddRow}>Add Row</button>
         </>
     );
 };
