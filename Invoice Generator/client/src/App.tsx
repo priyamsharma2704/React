68480f2716227886359647c2979fab09c078dc58
priyamsharma2704 <priyamsharma2704@gmail.com>
2025-03-14 17:57:43 -0400

    working on New Invoice Modal

--- Invoice Generator/client/src/App.tsx
+++ Invoice Generator/client/src/App.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+    const [showAddInvoice, setShowAddInvoice] = useState(false);
+
+    function showNewInvoiceModal() {
+        setShowAddInvoice(!showAddInvoice);
+    }
+
     return (
         <>
             <div className="container">
@@ -8,9 +15,14 @@ function App() {
                 <div className="info">
                     <div className="count">There are - {} invoices</div>
                     <div className="filter">Filter</div>
-                    <div className="newInvoice">Add Invoice</div>
+                    <div className="newInvoice">
+                        <button onClick={showNewInvoiceModal}>
+                            New Invoice
+                        </button>
+                    </div>
                 </div>
             </div>
+            {showAddInvoice ? <div className="newInvoiceModal">asd</div> : null}
         </>
     );
 }
