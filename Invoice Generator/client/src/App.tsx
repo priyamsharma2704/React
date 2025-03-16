933465b2f6bb81e1c7bf5d820927ec39aeadad39
priyam sharma <priyamsharma2704@gmail.com>
2025-03-16 16:41:32 -0400

    css updated

--- Invoice Generator/client/src/App.tsx
+++ Invoice Generator/client/src/App.tsx
@@ -8,6 +8,9 @@ function App() {
         setShowAddInvoice(!showAddInvoice);
     }
 
+    function addItem() {
+        console.log("add item");
+    }
     return (
         <>
             <div className="container">
@@ -22,7 +25,72 @@ function App() {
                     </div>
                 </div>
             </div>
-            {showAddInvoice ? <div className="newInvoiceModal">asd</div> : null}
+            {showAddInvoice ? (
+                <div className="newInvoiceModal">
+                    <div>
+                        <span className="countrySpan">Bill From</span>
+                    </div>
+
+                    <input
+                        className="country"
+                        type="text"
+                        placeholder="Country"
+                    />
+                    <div className="addressDiv">
+                        <input
+                            className="address"
+                            type="text"
+                            placeholder="City"
+                        />
+                        <input
+                            className="address"
+                            type="text"
+                            placeholder="State"
+                        />
+                        <input
+                            className="address"
+                            type="text"
+                            placeholder="Postal Code"
+                        />
+                    </div>
+                    <br></br>
+                    <div>
+                        <span>Bill To</span>
+                    </div>
+                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Street Name" />
+                    <div className="addressDiv">
+                        <input
+                            type="text"
+                            className="address"
+                            placeholder="City"
+                        />
+                        <input
+                            type="text"
+                            className="address"
+                            placeholder="State"
+                        />
+                        <input
+                            type="text"
+                            className="address"
+                            placeholder="Postal Code"
+                        />
+                    </div>
+                    <div className="dates">
+                        <input className="date" type="date" />
+                        <input className="dateValidity" type="text" />
+                        {/* make it drop down */}
+                    </div>
+                    <input type="text" placeholder="Project Description" />
+                    <br></br>
+                    <div>
+                        <span>Item List</span>
+                    </div>
+                    <button onClick={addItem}>Add Item</button>
+                    <div></div>
+                </div>
+            ) : null}
         </>
     );
 }
